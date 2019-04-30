fcf9e6567fdd2efc92717436c60bd64d67e1df9b
JetStarBlues <JetStarBlues@users.noreply.github.com>
2019-04-29 18:59:36 -0600

    cleanup

--- src/mkfs.c
+++ src/mkfs.c
@@ -81,7 +81,7 @@ int main ( int argc, char *argv[] )
 	struct dirent de;
 	char          buf [ BSIZE ];
 	struct dinode din;
-	char*         fileName;  // JK, find filename
+	char*         filename;  // JK, find filename
 
 
 	static_assert( sizeof( int ) == 4, "Integers must be 4 bytes!" );
@@ -134,14 +134,16 @@ int main ( int argc, char *argv[] )
 		nblocks, FSSIZE 
 	);
 
-	freeblock = nmeta;  // the first free block that we can allocate
+	// the first free block that we can allocate
+	freeblock = nmeta;
 
+	// Write zeroes to entire fs ??
 	for ( i = 0; i < FSSIZE; i += 1 )
 	{
 		wsect( i, zeroes );
 	}
 
-	//
+	// Write super block
 	memset( buf, 0, sizeof( buf ) );
 
 	memmove( buf, &sb, sizeof( sb ) );
@@ -149,13 +151,13 @@ int main ( int argc, char *argv[] )
 	wsect( 1, buf );
 
 
-	//
+	// Create root inode
 	rootino = ialloc( T_DIR );
 
 	assert( rootino == ROOTINO );
 
 
-	//
+	// Create "." directory entry for root
 	bzero( &de, sizeof( de ) );
 
 	de.inum = xshort( rootino );
@@ -165,7 +167,7 @@ int main ( int argc, char *argv[] )
 	iappend( rootino, &de, sizeof( de ) );
 
 
-	//
+	// Create ".." directory entry for root
 	bzero( &de, sizeof( de ) );
 
 	de.inum = xshort( rootino );
@@ -175,10 +177,10 @@ int main ( int argc, char *argv[] )
 	iappend( rootino, &de, sizeof( de ) );
 
 
+	// User files
 	for ( i = 2; i < argc; i += 1 )
 	{
-		// assert( index( argv[ i ], '/' ) == 0 );  // JK, huh... UPROGS binaries in bin/ ...
-
+		// Open file
 		if ( ( fd = open( argv[ i ], 0 ) ) < 0 )
 		{
 			perror( argv[ i ] );
@@ -186,47 +188,39 @@ int main ( int argc, char *argv[] )
 			exit( 1 );
 		}
 
-		// Skip leading _ in name when writing to file system.
-		// The binaries are named _rm, _cat, etc. to keep the
-		// build operating system from trying to execute them
-		// in place of system binaries like rm and cat.
-		/*
-			if ( argv[ i ][ 0 ] == '_' )
-			{
-				++argv[ i ];
-			}
+		// Get the filename
+		/* Skip leading _ in name when writing to file system.
+		   The binaries are named _rm, _cat, etc. to keep the
+		   build operating system from trying to execute them
+		   in place of system binaries like rm and cat.
 		*/
+		filename = basename( argv[ i ] );  // JK, find filename
 
-		fileName = basename( argv[ i ] );  // JK, find filename (no subdirectories)
-
-		if ( fileName )
+		if ( filename )
 		{
-			if ( fileName[ 0 ] == '_' )
+			if ( filename[ 0 ] == '_' )
 			{
-				fileName += 1;  // skip the char...
-
-				// printf( "aha! %s\n", fileName );
+				filename += 1;  // skip the char...
 			}
-			/*else
-			{
-				printf( "doh! %s\n", argv[ i ] );
-			}*/
+			// else {}
 		}
 
+
+		// Create an inode for the file
 		inum = ialloc( T_FILE );
 
 
-		//
+		// Create a directory entry for the file
 		bzero( &de, sizeof( de ) );
 
 		de.inum = xshort( inum );
 
-		// strncpy( de.name, argv[ i ], DIRNAMESZ );
-		strncpy( de.name, fileName, DIRNAMESZ );
+		strncpy( de.name, filename, DIRNAMESZ );
 
 		iappend( rootino, &de, sizeof( de ) );
 
 
+		// Write the file's contents to the inode's data blocks
 		while ( ( cc = read( fd, buf, sizeof( buf ) ) ) > 0 )
 		{
 			iappend( inum, buf, cc );
@@ -235,7 +229,7 @@ int main ( int argc, char *argv[] )
 		close( fd );
 	}
 
-	// fix size of root inode dir
+	// Fix size of root inode dir ??
 	rinode( rootino, &din );
 
 	off = xint( din.size );
@@ -246,6 +240,7 @@ int main ( int argc, char *argv[] )
 	winode( rootino, &din );
 
 
+	// Create bitmap
 	balloc( freeblock );
 
 	exit( 0 );
@@ -263,6 +258,22 @@ void wsect ( uint sec, void *buf )
 	if ( write( fsfd, buf, BSIZE ) != BSIZE )
 	{
 		perror( "write" );
+
+		exit( 1 );
+	}
+}
+
+void rsect ( uint sec, void *buf )
+{
+	if ( lseek( fsfd, sec * BSIZE, 0 ) != sec * BSIZE )
+	{
+		perror( "lseek" );
+
+		exit( 1 );
+	}
+	if ( read( fsfd, buf, BSIZE ) != BSIZE )
+	{
+		perror( "read" );
 
 		exit( 1 );
 	}
@@ -300,21 +311,6 @@ void rinode ( uint inum, struct dinode *ip )
 	*ip = *dip;
 }
 
-void rsect ( uint sec, void *buf )
-{
-	if ( lseek( fsfd, sec * BSIZE, 0 ) != sec * BSIZE )
-	{
-		perror( "lseek" );
-
-		exit( 1 );
-	}
-	if ( read( fsfd, buf, BSIZE ) != BSIZE )
-	{
-		perror( "read" );
-
-		exit( 1 );
-	}
-}
 
 uint ialloc ( ushort type )
 {
@@ -322,6 +318,7 @@ uint ialloc ( ushort type )
 	uint          inum;
 
 	inum = freeinode;
+
 	freeinode += 1;
 
 	bzero( &din, sizeof( din ) );
@@ -344,11 +341,11 @@ void balloc ( int used )
 
 	assert( used < BSIZE * 8 );
 
-	bzero( buf, BSIZE );
+	bzero( buf, BSIZE );  // clear all bits
 
 	for ( i = 0; i < used; i += 1 )
 	{
-		buf[ i / 8 ] = buf[ i / 8 ] | ( 1 << ( i % 8 ) );
+		buf[ i / 8 ] = buf[ i / 8 ] | ( 1 << ( i % 8 ) );  // if used, set bit
 	}
 
 	printf( "balloc: write bitmap block at sector %d\n", sb.bmapstart );
